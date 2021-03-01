57359cd2eea2bb0c1514695990cf4708db627227
CamilleAbella <camille.abella@hotmail.fr>
2021-03-01 21:25:39 +0100

    add an eval command

--- index.php
+++ index.php
@@ -2,12 +2,21 @@
 
 include __DIR__.'/vendor/autoload.php';
 
-use Discord\Discord;
 use Dotenv\Dotenv;
+use Discord\Discord;
+use Discord\Parts\Channel\Message;
 
 Dotenv::createImmutable(__DIR__)->load();
 
 define("PREFIX", $_ENV["PREFIX"]);
+
+function is(string $command, string $name): bool {
+    return str_starts_with($command, $name);
+}
+
+function crop(string $command, string $name): string {
+    return substr($command, strlen($name));
+}
 
 try {
     $client = new Discord([
@@ -18,10 +27,33 @@ try {
         echo "Bot is ready!", PHP_EOL;
 
         // Listen for messages.
-        $discord->on('message', function (\Discord\Parts\Channel\Message $message) {
+        $discord->on('message', function (Message $message) {
+            if($message->type != Message::TYPE_NORMAL)
+                return;
+
             if(str_starts_with($message->content, PREFIX)){
-                $command = substr($message->content, 1);
-                $message->reply("The command is $command");
+                $cmd = substr($message->content, 1);
+
+                // Handle eval command
+                if(is($cmd, "eval")){
+                    if($message->author->id == "352176756922253321"){
+                        try {
+                            $result = eval(crop($cmd, "eval").';');
+
+                            if(is_null($result)){
+                                $message->channel->sendMessage("Done.");
+                            }else{
+                                $stringResult = strval($result);
+                                $message->channel->sendMessage("```php\n$stringResult\n```");
+                            }
+                        }catch (ParseError $error){
+                            $errorMessage = $error->getMessage();
+                            $message->channel->sendMessage("```pbp\nParseError: $errorMessage\n```");
+                        }
+                    }else{
+                        $message->channel->sendMessage("Nope.");
+                    }
+                }
             }
         });
     });
